aacb2248d3cf7977ec86f54b17bf62702a0181f3
will-cesar <willcesar010421@gmail.com>
2021-08-28 09:57:18 -0300

    ajuste gitignore

--- poke-app/src/app/modules/pokemon/pokemon.module.ts
+++ poke-app/src/app/modules/pokemon/pokemon.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ListComponent } from './pages/list/list.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { PokemonRoutingModule } from './pokemon-routing.module';
 
 @NgModule({
   declarations: [ListComponent, DetailsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, PokemonRoutingModule],
 })
 export class PokemonModule {}
