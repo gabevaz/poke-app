9d60e255fd207414f546fa58d76ce82d029f72a3
will-cesar <willcesar010421@gmail.com>
2021-08-28 22:38:21 -0300

    Import da paginação e módulo de forms no módulo de pokemon

--- poke-app/src/app/modules/pokemon/pokemon.module.ts
+++ poke-app/src/app/modules/pokemon/pokemon.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { ListComponent } from './pages/list/list.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { PokemonRoutingModule } from './pokemon-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ListComponent, DetailsComponent],
-  imports: [CommonModule, HttpClientModule, PokemonRoutingModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    PokemonRoutingModule,
+    NgxPaginationModule,
+    FormsModule,
+  ],
 })
 export class PokemonModule {}
