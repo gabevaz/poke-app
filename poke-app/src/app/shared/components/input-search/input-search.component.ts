98bb3b9ff2c6bae4b8e1e9e4dd2a966508d128d6
will-cesar <willcesar010421@gmail.com>
2021-08-29 13:33:31 -0300

    Criação do componente de busca

---
+++ poke-app/src/app/shared/components/input-search/input-search.component.ts
@@ -0,0 +1,52 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { debounceTime, map, distinctUntilChanged } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+
+@Component({
+  selector: 'input-search',
+  templateUrl: './input-search.component.html',
+  styleUrls: ['./input-search.component.scss'],
+})
+export class InputSearchComponent implements OnInit {
+  @ViewChild('fieldSearch', { static: true }) fieldSearch: ElementRef;
+  @Output() inputValue = new EventEmitter<string>();
+
+  inputSearchValue: any = '';
+
+  constructor() {}
+
+  private sendValue(): void {
+    this.inputValue.emit(this.inputSearchValue);
+  }
+
+  cleanInputSearch(): void {
+    this.inputSearchValue = '';
+    this.sendValue();
+  }
+
+  ngOnInit(): void {
+    this.setEventToInput();
+  }
+
+  setEventToInput(): void {
+    fromEvent(this.fieldSearch.nativeElement, 'keyup')
+      .pipe(
+        map((event: any) => {
+          return event.target.value;
+        }),
+        debounceTime(1000),
+        distinctUntilChanged()
+      )
+      .subscribe((text: string) => {
+        this.sendValue();
+      });
+  }
+}
--- poke-app/src/app/shared/shared.module.ts
+++ poke-app/src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { HeaderHomeComponent } from './components/header-home/header-home.component';
 import { NoResultsComponent } from './components/no-results/no-results.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
 
 @NgModule({
-  declarations: [HeaderHomeComponent, NoResultsComponent],
-  imports: [CommonModule],
-  exports: [HeaderHomeComponent, NoResultsComponent],
+  declarations: [HeaderHomeComponent, NoResultsComponent, InputSearchComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [HeaderHomeComponent, NoResultsComponent, InputSearchComponent],
 })
 export class SharedModule {}
