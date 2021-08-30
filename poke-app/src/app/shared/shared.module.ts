d726584d7d8e49d5a46be46f6b2965bac9502622
will-cesar <willcesar010421@gmail.com>
2021-08-30 09:39:06 -0300

    Criação do componente de carregamento

--- poke-app/src/app/shared/shared.module.ts
+++ poke-app/src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { NoResultsComponent } from './components/no-results/no-results.component';
 import { InputSearchComponent } from './components/input-search/input-search.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-  declarations: [NoResultsComponent, InputSearchComponent],
+  declarations: [NoResultsComponent, InputSearchComponent, LoaderComponent],
   imports: [CommonModule, FormsModule],
-  exports: [NoResultsComponent, InputSearchComponent],
+  exports: [NoResultsComponent, InputSearchComponent, LoaderComponent],
 })
 export class SharedModule {}
